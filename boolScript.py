19c8d72528426a0d11a21a8b18582ebd779fe723
student <student@info-02.cshl.edu>
2022-10-12 16:49:58 -0400

    adding boolScript.py

---
+++ boolScript.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import math
+
+
+valVar = 3
+# Check if the variable value is true
+  
+if bool(valVar):
+  print("This is TRUE")
+else:
+  print("Not True")
+
+
+
+
+
+
+
+
+
+
+
